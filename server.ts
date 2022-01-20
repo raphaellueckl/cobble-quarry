c45f8a3a56a35dee9179568ef9c5e0e0ad55c2d5
raphaellueckl <codepleb4@gmail.com>
2022-01-20 23:05:40 +0100

    init - create running server

---
+++ server.ts
@@ -0,0 +1,70 @@
+import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
+const app = new Application();
+const port = 3000;
+
+app.use(
+  oakCors({
+    origin: "*",
+  })
+);
+
+const router = new Router();
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+
+router
+  .get("/", async (ctx) => {
+    await send(ctx, 'index.html', {
+      root: `${Deno.cwd()}/public`,
+    });
+  })
+  .get("/server/start", (ctx) => {
+    // startServer();
+    ctx.response.body = 'Starting up...';
+  })
+  .get("/server/stop", (ctx) => {
+    // stopServer();
+    ctx.response.body = 'Shutting down...';
+  })
+  .post("/server/command", async (ctx) => {
+    await p.stdin.write(encoder.encode(values.command + "\n"));
+  })
+
+
+// const startServer = async () => {
+//   p = Deno.run({
+//     cmd: ["java", "-Xmx1024M", "-Xms1024M", "-jar", "server.jar", "nogui"],
+//     stdout: "piped",
+//     stdin: "piped",
+//     stderr: "piped",
+//   });
+
+//   Deno.copy(p.stdout, Deno.stdout);
+//   Deno.copy(p.stdout, Deno.stdout);
+// }
+
+// const stopServer = async () => {
+//   if (p !== null) {
+//     try {
+//       await p.stdin.write(encoder.encode("/stop"));
+//     } catch {
+//       console.log("closing the server failed");
+//     }
+
+//     await p.stdin.close();
+//     serverState = "Shut down minecraft."
+//   }
+// }
+
+app.use(async (ctx, next) => {
+  console.log(`${ctx.request.method} ${ctx.request.url}`);
+  await next();
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen({ port });
+
+console.log(`API listening on: 'localhost:${port}'`);
