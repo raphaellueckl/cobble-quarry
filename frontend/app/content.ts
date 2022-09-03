6634eb504adf8bf919fa2026e484bcd71e7e1f20
raphaellueckl <codepleb4@gmail.com>
2022-09-03 23:17:12 +0200

    frontend first steps

--- frontend/app/content.ts
+++ frontend/app/content.ts
@@ -4,12 +4,20 @@ import { customElement, property } from "lit/decorators.js";
 @customElement("cq-content")
 export class Content extends LitElement {
   static styles = css`
-    div {
-      background-color: red;
+    .content {
+      display: flex;
+      flex-direction: column;
     }
   `;
 
   render() {
-    return html` <div>Me the content</div> `;
+    return html`
+      <div class="content">
+        <h2>Logs</h2>
+        <textarea></textarea>
+        <h2>Commands</h2>
+        <input />
+      </div>
+    `;
   }
 }
