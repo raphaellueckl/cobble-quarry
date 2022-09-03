0c4935c77bc4750cc7c81382e286daf83bf98711
raphaellueckl <codepleb4@gmail.com>
2022-09-03 21:16:29 +0200

    refactor server

--- backend/server.ts
+++ backend/server.ts
@@ -4,7 +4,7 @@ import * as mod from "https://deno.land/std@0.104.0/io/util.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
 const app = new Application();
 const port = 3000;
-Deno.env.set("LD_LIBRARY_PATH", ".")
+Deno.env.set("LD_LIBRARY_PATH", ".");
 
 app.use(
   oakCors({
@@ -16,16 +16,24 @@ const router = new Router();
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
 
-const STOPPED = 0;
-const STARTED = 1;
-const PROGRESS = 2;
+const STARTED = "STARTED";
+const ALREADY_STARTED = "ALREADY_STARTED";
+const STOPPED = "STOPPED";
+const ALREADY_STOPPED = "ALREADY_STOPPED";
+const PROGRESS = "PROGRESS";
+const SUCCESS = "SUCCESS";
+const FAILED = "FAILED";
 
-let mcProcess = null;//: Process<any>|null;
+let mcProcess = null; //: Process<any>|null;
 let serverState = STOPPED;
+
+interface ServerState {
+  state: string;
+}
 
 router
   .get("/", async (ctx) => {
-    await send(ctx, 'index.html', {
+    await send(ctx, "index.html", {
       root: `${Deno.cwd()}/public`,
     });
   })
@@ -34,34 +42,38 @@ router
       serverState = PROGRESS;
       startServer();
       serverState = STARTED;
+      ctx.response.body = { state: STARTED };
+      return;
     }
-    ctx.response.body = 'Starting server...';
+    ctx.response.body = { state: ALREADY_STARTED };
   })
   .get("/stop", async (ctx) => {
     if (mcProcess !== null) {
       serverState = PROGRESS;
       await stopServer();
       serverState = STOPPED;
+      ctx.response.body = { serverState: STOPPED };
+      return;
     }
-    ctx.response.body = {serverState: STOPPED};
+    ctx.response.body = { serverState: ALREADY_STOPPED };
   })
   .post("/command", async (ctx) => {
-    if (mcProcess !== null) {
+    if (mcProcess !== null && serverState !== STOPPED) {
       const command = await ctx.request.body().value;
       console.log(`Executing: /${command}`);
       await mcProcess.stdin.write(encoder.encode(command + "\n"));
+      ctx.response.body = { state: SUCCESS };
+      return;
     }
-  })
+    ctx.response.body = { state: FAILED };
+  });
 
-const messageObserver = async (
-  reader: Deno.Reader,
-  writer: Deno.Writer,
-) => {
+const messageObserver = async (reader: Deno.Reader, writer: Deno.Writer) => {
   const encoder = new TextEncoder();
   for await (const line of readLines(reader)) {
     await mod.writeAll(writer, encoder.encode(`${line}\n`));
   }
-}
+};
 
 const startServer = async () => {
   mcProcess = Deno.run({
@@ -73,7 +85,7 @@ const startServer = async () => {
 
   messageObserver(mcProcess.stdout, Deno.stdout);
   messageObserver(mcProcess.stderr, Deno.stderr);
-}
+};
 
 const stopServer = async () => {
   if (mcProcess !== null) {
@@ -86,9 +98,9 @@ const stopServer = async () => {
 
     await mcProcess.stdin.close();
   } else {
-    console.log("Server is not running!")
+    console.log("Server is not running!");
   }
-}
+};
 
 const timer = (delay) =>
   new Promise((resolve) => {
