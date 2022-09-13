ea837d019bfceae464bfcd7661afd0f53eac9ec7
raphaellueckl <codepleb4@gmail.com>
2022-09-13 22:56:15 +0200

    improve logging and add timestamped backups

--- backend/server.ts
+++ backend/server.ts
@@ -6,18 +6,20 @@ import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 Deno.env.set("LD_LIBRARY_PATH", ".");
 
-const IDLE_SERVER_THRESHOLD_MINUTES = 30;
+const ADMIN_PW = "ADMIN_PW";
+const MOD_PW = "MOD_PW";
+const BACKUP_PATH = "BACKUP_PATH";
+const AUTO_SHUTDOWN = "AUTO_SHUTDOWN";
 
-const adminPW: string = Deno.env.get("ADMIN_PW") || "";
-const modPW: string = Deno.env.get("MOD_PW") || "";
-let backupPath: string = Deno.env.get("BACKUP_PATH") || "";
-console.log("Backup Path: ", backupPath);
-let shutdownOnIdle: string = Deno.env.get("AUTO_SHUTDOWN") || "";
-console.log(
-  shutdownOnIdle
-    ? `Server (Computer) will shut down if no players for ${IDLE_SERVER_THRESHOLD_MINUTES} minutes. To avoid that, do not set 'AUTO_SHUTDOWN'.`
-    : "Server (Computer) is set to not automatically shutdown, if there are no players. Provide 'AUTO_SHUTDOWN=yes' if you want that."
-);
+const env_adminPW: string = Deno.env.get(ADMIN_PW) || "";
+const env_modPW: string = Deno.env.get(MOD_PW) || "";
+let env_backupPath: string = Deno.env.get(BACKUP_PATH) || ""; // /home/username/minecraft
+let env_shutdownOnIdle: string = Deno.env.get(AUTO_SHUTDOWN) || ""; // y
+
+// Fix lazy user input
+if (env_backupPath && env_backupPath[env_backupPath.length - 1] !== "/") {
+  env_backupPath += "/";
+}
 
 const app = new Application();
 const port = 3000;
@@ -30,6 +32,7 @@ const PROGRESS = "PROGRESS";
 const SUCCESS = "SUCCESS";
 const FAILED = "FAILED";
 
+const IDLE_SERVER_MINUTES_THRESHOLD = 30;
 const ONE_MINUTE = 60 * 1000;
 let serverIdleMinutes = 0;
 
@@ -38,12 +41,34 @@ let serverState = PROGRESS;
 let playerCount = 0;
 let backupOnNextOccasion = false;
 
-const isAuthenticatedAsAdmin = (ctx): boolean =>
-  !adminPW || ctx.request.headers.get("pw") === adminPW;
+let logQueue: String[] = [];
 
-const isAuthenticatedAsMod = (ctx): boolean =>
-  !modPW ||
-  ctx.request.headers.get("pw") === modPW ||
+const log = (content: string, isMCMessage = false) => {
+  logQueue.push(isMCMessage ? "[Minecraft] " : "[Cobble]    " + content);
+  console.log(isMCMessage ? "[Minecraft] " : "[Cobble]    " + content);
+};
+
+log("Backup Path: " + env_backupPath);
+log(
+  env_shutdownOnIdle
+    ? `Server (Computer) will shut down if no players for ${IDLE_SERVER_MINUTES_THRESHOLD} minutes. To avoid that, do not set '${AUTO_SHUTDOWN}'.`
+    : `Server (Computer) is set to not automatically shutdown, if there are no players. Provide '${AUTO_SHUTDOWN}=yes' if you want that.`
+);
+if (!env_adminPW)
+  log(
+    `No ADMIN password given. Provide the variable '${ADMIN_PW}' with a freely chosen password on startup of cobble-quarry, otherwise everyone with access to the website can execute all actions.`
+  );
+if (!env_modPW)
+  log(
+    `No MOD password given. Provide the variable '${MOD_PW}' with a freely chosen password on startup of cobble-quarry, otherwise everyone with access to the website can execute some actions.`
+  );
+
+const isAuthenticatedAsAdmin = (ctx: any): boolean =>
+  !env_adminPW || ctx.request.headers.get("pw") === env_adminPW;
+
+const isAuthenticatedAsMod = (ctx: any): boolean =>
+  !env_modPW ||
+  ctx.request.headers.get("pw") === env_modPW ||
   isAuthenticatedAsAdmin(ctx);
 
 router
@@ -77,20 +102,19 @@ router
       !(serverState === STOPPED || serverState === PROGRESS)
     ) {
       const command = await ctx.request.body().value;
-      console.log(`Executing: /${command}`);
+      log(`Executing: /${command}`);
       await mcProcess?.stdin?.write(encoder.encode(command + "\n"));
       ctx.response.body = { state: SUCCESS };
       return;
     }
     ctx.response.body = { state: FAILED };
   })
-  .post("/backup", async () => {
-    if (isAuthenticatedAsMod(ctx) && backupPath) {
+  .post("/backup-and-restart", async (ctx) => {
+    if (isAuthenticatedAsMod(ctx) && env_backupPath) {
       await stopServer(20);
-      await backupServer();
       startServer();
     } else {
-      console.log('No "BACKUP_PATH" environment variable given.');
+      log(`No '${BACKUP_PATH}' environment variable given.`);
     }
   });
 
@@ -101,6 +125,7 @@ const messageObserver = async (
   if (!reader) return;
   const encoder = new TextEncoder();
   for await (const line of readLines(reader)) {
+    log(line, true);
     await conversion.writeAll(writer, encoder.encode(`${line}\n`));
     if (
       line.includes("======================================================")
@@ -110,8 +135,7 @@ const messageObserver = async (
       ++playerCount;
       backupOnNextOccasion = true;
     } else if (line.includes(" Player disconnected: ")) {
-      if (--playerCount === 0) {
-      }
+      --playerCount;
     } else if (line.includes("Quit correctly")) {
       serverState = STOPPED;
     }
@@ -131,25 +155,31 @@ const startServer = async () => {
 };
 
 const backupServer = async () => {
-  await stdCopy.copy(Deno.cwd(), backupPath);
+  if (backupOnNextOccasion) {
+    await stdCopy.copy(Deno.cwd(), env_backupPath + new Date().toISOString());
+    backupOnNextOccasion = false;
+  } else {
+    log("No backup was made due to no player having joined since last backup.");
+  }
 };
 
 const stopServer = async (offset?: number) => {
   try {
     if (offset) {
       for (let i = offset; i > 0; i--) {
+        log(`Server stop in ${i}`);
         await mcProcess?.stdin?.write(encoder.encode(`Server stop in ${i}\n`));
         await timer(1000);
       }
     }
+    log("stop");
     await mcProcess?.stdin?.write(encoder.encode("stop\n"));
     await mcProcess?.stdin?.close();
     playerCount = 0;
 
-    if (backupOnNextOccasion) await backupServer();
-    backupOnNextOccasion = false;
+    await backupServer();
   } catch {
-    console.log("Closing or backupping the server failed!");
+    log("Closing or backupping the server failed!");
   }
 };
 
@@ -165,8 +195,8 @@ const timer = (delayInMillis: number) =>
 const shutdownOnIdleWatcher = async () => {
   while (true) {
     await timer(ONE_MINUTE);
-    if (shutdownOnIdle && playerCount === 0) {
-      if (++serverIdleMinutes === IDLE_SERVER_THRESHOLD_MINUTES) {
+    if (env_shutdownOnIdle && playerCount === 0) {
+      if (++serverIdleMinutes === IDLE_SERVER_MINUTES_THRESHOLD) {
         await stopServer();
         shutdownHost();
       }
@@ -180,7 +210,7 @@ startServer();
 shutdownOnIdleWatcher();
 
 app.use(async (ctx, next) => {
-  console.log(`${ctx.request.method} ${ctx.request.url}`);
+  log(`${ctx.request.method} ${ctx.request.url}`);
   await next();
 });
 
@@ -205,4 +235,4 @@ app.use(async (context, next) => {
   }
 });
 
-console.log(`Listening on: localhost:${port}`);
+log(`Listening on: localhost:${port}`);
